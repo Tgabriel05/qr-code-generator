2181f3c1a3b587acb10be399dbbae21e2f4895c3
Tgabriel05 <Bmosquera0510@gmail.com>
2024-04-05 21:48:11 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,65 @@
+
+const qrBtn = document.getElementById('qr-btn'),
+      urlInput = document.getElementById('url-input'),
+      qrCodeDisplay = document.getElementById('qr-code'),
+      downloadBtn = document.getElementById('download-btn'),
+      copyBtn = document.getElementById('copy-btn'),
+      page1Display = document.getElementById('page1'),
+      page2Display = document.getElementById('page2'),
+      bgLogo = document.getElementById('bg');
+
+//API QR CODE
+
+
+function getQrCode(){
+  if(urlInput.value === ''){
+    urlInput.classList.add('error');
+    setTimeout(()=>{
+      urlInput.classList.remove('error');
+    },1000)
+    return;
+  }
+  var qrCode = new QRCode(qrCodeDisplay, {
+    text: urlInput.value,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    width: 185,
+    height:200,
+    correctLevel: QRCode.CorrectLevel.H
+  });
+  page1Display.style.display = 'none';
+  page2Display.style.display = 'block';
+  bgLogo.style.display = 'block';
+  console.log(qrCode);
+
+
+};
+function downloadQR(){
+  let qr_code_img = document.querySelector('#qr-code img'),
+      qr_code_canvas = document.querySelector('canvas');
+
+  downloadBtn.href = qr_code_img.getAttribute('src') || qr_code_canvas.toDataURL();
+  downloadBtn.download = `${urlInput.value}.png`;
+};
+
+function copyQuote(){
+  urlInput.select();
+  navigator.clipboard.writeText(urlInput.value);
+  copyBtn.innerHTML = 'Copied <i class="fa-solid fa-check"></i>';
+  setTimeout(()=>{
+    copyBtn.innerHTML = `Share <i class="fa fa-link" ></i>`
+  },2000);
+  
+}
+//CLICKING BUTTONS
+qrBtn.addEventListener('click', getQrCode);
+urlInput.addEventListener('keydown', (e)=>{
+  if(e.key === 'Enter'){
+    e.preventDefault();
+    getQrCode();
+  }
+});
+downloadBtn.addEventListener('click', downloadQR);
+copyBtn.addEventListener('click', copyQuote);
+
+
